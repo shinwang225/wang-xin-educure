6e5efea44537b620f2fc7a8d5cfe331c1d0107cd
shinwang225 <wangxinjp0225@gmail.com>
2025-06-12 22:47:24 +0900

    Add files via upload
    修正しましたので、ご確認お願いいたします。

--- 2-7/src/Main6.java
+++ 2-7/src/Main6.java
@@ -1,10 +1,17 @@
+import java.util.Scanner;  
 public class Main6 {
-    public static void main(String[] args){
-        String currentExp1 ="1000";
-        int currentExp2 = Integer.parseInt(currentExp1);
-        int bounsExp = currentExp2 * 25 /100;
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
-        System.out.println("現在の経験値を入力してください：" + currentExp2);
-        System.out.println("ボーナス後の経験値：" + (currentExp2 + bounsExp) );
+        System.out.println("現在の経験値を入力してください：");
+        String exp = scanner.nextLine();
+        int currentExp = Integer.parseInt(exp);
+
+        double bouns = currentExp * 0.25;
+        int bounsExp = (int)bouns;
+        int expAfterbonus = currentExp + bounsExp;
+        
+        System.out.println("ボーナス後の経験値：" + expAfterbonus);
+        scanner.close();
     }    
 }
--- 2-7/src/Main8.java
+++ 2-7/src/Main8.java
@@ -1,13 +1,23 @@
+import java.util.Scanner;  
 public class Main8 {
-    public static void main(String[] args){
-        int attack = 50;
-        int denfense = 20;
-        int normalDamage = 30;
-        double criticalDamage = 1.5 * normalDamage;
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("攻撃力を入力してください：");
+        String attack = scanner.nextLine(); 
+        int attackValue = Integer.parseInt(attack);
 
-        System.out.println("攻撃力を入力してください：" + attack);
-        System.out.println("防御力を入力してください：" + denfense);
+        System.out.println("防御力を入力してください：");
+        String denfense = scanner.nextLine(); 
+        int denfenseValue = Integer.parseInt(denfense);
+
+        int normalDamage = attackValue - denfenseValue;
+        double criticalDamage1 = 1.5 * normalDamage;
+        int criticalDamage2 = (int)criticalDamage1;
+        
         System.out.println("通常ダメージ：" + normalDamage);
-        System.out.println("クリティカルダメージ：" + (int)criticalDamage);
+        System.out.println("クリティカルダメージ：" + criticalDamage2);
+
+        scanner.close();
     }
 }
