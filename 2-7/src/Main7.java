04e30b0d40df72bed78318a5e0ee9a8bc7f2dbbf
shinwang225 <wangxinjp0225@gmail.com>
2025-06-14 19:54:15 +0900

    Update Main7.java

--- 2-7/src/Main7.java
+++ 2-7/src/Main7.java
@@ -1,13 +1,10 @@
 public class Main7 {
     public static void main(String[] args){
-        int HP = 100;
-        HP--;
-        System.out.println("攻撃1回目：残り体力" + HP);
+        int Health = 100;
+        System.out.println("攻撃1回目：残り体力" + Health--);
 
-        HP--;
-        System.out.println("攻撃2回目：残り体力" + HP);
+        System.out.println("攻撃2回目：残り体力" + Health--);
 
-        HP--;
-        System.out.println("攻撃3回目：残り体力" + HP);
+        System.out.println("攻撃3回目：残り体力" + Health--);
     }
 }
