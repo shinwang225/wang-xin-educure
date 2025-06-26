34acebfc7d45a605d6099ac00eb9a10fc9d391e6
shinwang225 <wangxinjp0225@gmail.com>
2025-06-27 06:37:49 +0900

    Update Main2.java

--- 2-21/src/Main2.java
+++ 2-21/src/Main2.java
@@ -1,6 +1,8 @@
 public class Main2 {
     public static void calculateCircle(double radius) {
         // ここにコードを書いてください
+        System.out.println("半径が" + radius + "の場合:");
+
         double area = Math.PI * radius * radius;
         double circumference = 2 * Math.PI * radius;
 
@@ -9,7 +11,6 @@ public class Main2 {
     }
     public static void main(String[] args) {
         double radius = 5.0;
-        System.out.println("半径が" + radius + "の場合:");
         calculateCircle(radius);
     }
 }
