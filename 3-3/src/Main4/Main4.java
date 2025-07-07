79dded4d6be042ffee76445734130b93cf744e7d
shinwang225 <wangxinjp0225@gmail.com>
2025-07-08 07:31:55 +0900

    Update Main4.java

--- 3-3/src/Main4/Main4.java
+++ 3-3/src/Main4/Main4.java
@@ -1,12 +1,13 @@
 public class Main4 {
     public static void main(String[] args) {
-        // ここにコードを書いてください
+        
         Weapon weapon = new Weapon();
         weapon.setWeapon("伝説の剣", 100);
 
         for (int i = 0; i < 10; i++) {
             weapon.use();
         }
+        weapon.use();
         weapon.repair();
         weapon.use();
     }
