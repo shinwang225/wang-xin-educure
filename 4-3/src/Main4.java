02eb67b7090fcac2eecb010860d078d7313c73c5
shinwang225 <wangxinjp0225@gmail.com>
2025-07-30 07:55:05 +0900

    Update Main4.java

--- 4-3/src/Main4.java
+++ 4-3/src/Main4.java
@@ -13,6 +13,18 @@ public class Main4 {
         this.startTime = start;
         this.endTime = end;
     }
+    
+    //追加：開始時間をフォーマットする
+    public String getFormattedStartTime() {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy年M月d日 HH:mm z");
+        return startTime.format(formatter);   
+    }
+
+    //追加：終了時間をフォーマットする
+    public String getFormattedEndTime() {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy年M月d日 HH:mm z");
+        return endTime.format(formatter);
+    }
 
     public String getEventDuration() {
         Duration duration = Duration.between(startTime, endTime);
@@ -40,8 +52,8 @@ public class Main4 {
         Main4 event1 = new Main4("サマーフェスティバル", start, end);
 
         System.out.println("イベント: " + event1.eventName);
-        System.out.println("開始: " + event1.startTime);
-        System.out.println("終了: " + event1.endTime);
+        System.out.println("開始: " + event1.getFormattedStartTime());
+        System.out.println("終了: " + event1.getFormattedEndTime());
         System.out.println("期間: " + event1.getEventDuration());
         System.out.println("現地時間: " + event1.getLocalTime());
         System.out.println("UTC時間: " + event1.getUtcTime());
