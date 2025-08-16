e54eaf3b66d1f3166f83cf53f7a31643013cb678
shinwang225 <wangxinjp0225@gmail.com>
2025-08-17 06:34:47 +0900

    Update Main4.java

--- 4-12/src/Main4/Main4.java
+++ 4-12/src/Main4/Main4.java
@@ -2,7 +2,7 @@ package Main4;
 
 public class Main4 {
     public static void main(String[] args) {
-        PartyMember pm1 = new PartyMember("元のキャラクター「アリス」");
+        PartyMember pm1 = new PartyMember("アリス");
         pm1.addSkill("ファイア");
         pm1.addSkill("ブリザード");
 
@@ -10,8 +10,8 @@ public class Main4 {
         pm2.addSkill("サンダー");
 
         System.out.println("--- 深いコピー後にスキル追加 ---");
-        pm1.showSkills();
-        pm2.showSkills();
+        pm1.showSkills("元のキャラクター");
+        pm2.showSkills("コピーしたキャラクター");
 
     }
 }
