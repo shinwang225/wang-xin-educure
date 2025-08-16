4d00457c501fdda74227ae84c17e7b0668e8992f
shinwang225 <wangxinjp0225@gmail.com>
2025-08-17 06:36:25 +0900

    Update Main1.java

--- 4-12/src/Main1/Main1.java
+++ 4-12/src/Main1/Main1.java
@@ -5,7 +5,7 @@ public class Main1 {
         
         System.out.println("アイテム情報:");
         GameItem item1 = new GameItem("回復薬", 100);
-        GameItem item2 = new GameItem("魔法の杖", 150);
+        GameItem item2 = new GameItem("魔法の杖", 1500);
 
         System.out.println(item1);
         System.out.println(item2);
