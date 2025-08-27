f18e69d3db6b5527a76b56b5cc93a5d2caea4fd2
shinwang225 <wangxinjp0225@gmail.com>
2025-08-28 08:22:06 +0900

    Add files via upload

---
+++ 4-20/src/Main4.java
@@ -0,0 +1,44 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardCopyOption;
+
+public class Main4 {
+    public static void main(String[] args) {
+        Path source = Path.of("source.txt");
+        Path backupDirectory = Path.of("new_backup");
+        Path archiveDirectory = Path.of("new_archive");
+
+        try {
+            Files.createDirectories(backupDirectory);
+            Files.createDirectories(archiveDirectory);
+
+            if (!Files.exists(source)) {
+                Files.writeString(source, "sourceファイルです");
+            }
+
+            Path backupFile = backupDirectory.resolve(source.getFileName());
+            Files.copy(source, backupFile, StandardCopyOption.REPLACE_EXISTING);
+            if (Files.exists(backupFile)) {
+                System.out.println("ファイルが 'backup/' にコピーされました。");
+                System.out.println("コピーの確認: 成功");
+            } else {
+                System.out.println("コピーの確認: 失敗");
+            }
+
+            Path archiveFile = archiveDirectory.resolve(backupFile.getFileName());
+            Files.move(backupFile, archiveFile, StandardCopyOption.REPLACE_EXISTING);
+            if (Files.exists(archiveFile)) {
+                System.out.println("ファイルが 'archive/' に移動されました。");
+                System.out.println("移動の確認: 成功");
+            } else {
+                System.out.println("移動の確認: 失敗");
+            }
+
+        } catch (IOException e) {
+            System.out.println("処理中にエラーが発生しました。");
+            e.printStackTrace();
+        }
+    }
+}
+
